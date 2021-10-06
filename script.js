cd7e65a995aec65222d2928dd8b279bc74cd3bf2
Ilyes Djari <ilyes.djari@student.ehb.be>
2021-10-06 21:35:05 +0200

    Working radio buttons without array

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 
 window.onload = () => {
     console.log("The page has been loaded succesfully");
-    listen();
 
+    let dishes = [];
+    dishes.push({
+        id: '1',
+        name: 'Burger and french fries',
+        price: '12'
+    }, {
+        id: '2',
+        name: 'Ceasar salad',
+        price: '10'
+    }, {
+        id: '3',
+        name: 'Milkshake',
+        price: '5'
+    });
+
+
+
+
+    listen();
 };
 
 function listen() {
@@ -11,7 +29,7 @@ function listen() {
         event.preventDefault(), alert('You sucessfully submited the form !');
         let name = document.getElementById('nameinput').value;
         let email = document.getElementById('emailinput').value;
-        let order = document.getElementById('orderinput').value;
+        let order = document.querySelector('input[name=order]:checked').value
         let orderDetails = {
             name,
             email,
@@ -20,5 +38,6 @@ function listen() {
         console.log(orderDetails);
         document.getElementById('messages').innerHTML =
             `The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.email}`;
+
     });
 };
