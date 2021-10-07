2090782078139a33bdf432b1271d50c8e8a41070
Ilyes Djari <ilyes.djari@student.ehb.be>
2021-10-07 10:30:50 +0200

    S1 is completly up to date

--- S1/script.js
+++ S1/script.js
@@ -2,8 +2,30 @@
 
 window.onload = () => {
     console.log("The page has been loaded succesfully");
-    listen();
 
+    let dishes = [];
+    dishes.push({
+        id: '1',
+        name: 'Burger and french fries',
+        price: '12€'
+    }, {
+        id: '2',
+        name: 'Ceasar salad',
+        price: '10€'
+    }, {
+        id: '3',
+        name: 'Milkshake',
+        price: '5€'
+    });
+
+    dishes.forEach(dishes =>
+        document.getElementById('menu').insertAdjacentHTML('beforeend', `
+        <div>
+                <input id="${dishes.id}" name="order" type="radio" value="${dishes.name}" checked>
+                <label for="${dishes.id}">${dishes.name} ${dishes.price}</label>
+            </div>
+        `));
+    listen();
 };
 
 function listen() {
@@ -11,14 +33,23 @@ function listen() {
         event.preventDefault(), alert('You sucessfully submited the form !');
         let name = document.getElementById('nameinput').value;
         let email = document.getElementById('emailinput').value;
-        let order = document.getElementById('orderinput').value;
+        let order = document.querySelector('input[name=order]:checked').value
         let orderDetails = {
             name,
             email,
             order
         }
-        console.log(orderDetails);
-        document.getElementById('messages').innerHTML =
-            `The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.email}`;
+        if (orderDetails.name != '' & orderDetails.email != '' & orderDetails.order != '') {
+            printOrder(orderDetails)
+        } else {
+            document.getElementById('messages').innerHTML = 'Please fill all details to place your order'
+        }
+
     });
 };
+
+function printOrder(orderDetails) {
+    document.getElementById('messages').innerHTML =
+        `The order for the customer ${orderDetails.name} is the following: ${orderDetails.order}.
+         The customer may be notified by email: ${orderDetails.email}`;
+}
