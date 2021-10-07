4864e94b2f15aace84e223e7d5b07c3e3f7ff59c
Ilyes Djari <ilyes.djari@student.ehb.be>
2021-10-07 10:18:40 +0200

    Beginning the exercices

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 "use strict";
 
-window.onload = () => {
-    console.log("The page has been loaded succesfully");
-    listen();
 
+let pokemons = []
+
+showPoke();
+
+function showPoke() {
+    console.log("start loading message");
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            pokemons.push(data.results)
+            pokemons.forEach()
+        })
 };
 
-function listen() {
-    document.getElementById('submitme').addEventListener("submit", function (event) {
-        event.preventDefault(), alert('You sucessfully submited the form !');
-        let name = document.getElementById('nameinput').value;
-        let email = document.getElementById('emailinput').value;
-        let order = document.getElementById('orderinput').value;
-        let orderDetails = {
-            name,
-            email,
-            order
-        }
-        console.log(orderDetails);
-        document.getElementById('messages').innerHTML =
-            `The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email:  ${orderDetails.email}`;
-    });
-};
+function retrieveThem() {
+    pokemons.forEach()
+}
